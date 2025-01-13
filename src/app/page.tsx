0144f690e2eedbc0768b71f622208f26452166ab
simjeeh <huzaifahsimjee@gmail.com>
2025-01-12 17:02:27 -0800

    fix home height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,13 @@ export default function Home() {
       <Header/>
       <div className="relative h-screen" style={{ backgroundImage: 'url(/banner.jpg)' }}>
         <div className="absolute bottom-0 w-full h-full bg-black bg-opacity-20">
-          <div className="absolute bottom-0 w-full h-4/5">
             <div className="grid grid-cols-9 text-white">
-              <div className="justify-self-center col-start-1 col-end-10 pb-[5rem]">
-                <Image src="/logo.svg" alt="Logo" width={200} height={200}/>\
+              <div className="justify-self-center col-start-1 col-end-10 h-[50vh] flex">
+                <Image src="/logo.svg" alt="Logo" width={200} height={200}/>
               </div>
               {content.sections.map((section: ContentSection) => (
-                <div key={`${content.title}-${section.title}`} className="col-start-5 col-span-5">
-                  <div className="p-4 text-6xl font-bold bg-white text-green-786">{section.title}</div>
+                <div key={`${content.title}-${section.title}`} className="col-start-3 col-span-5">
+                  <div className="p-4 text-5xl font-bold bg-white text-green-786">{section.title}</div>
                   {section.paragraphs.map((sectionParagraph: string, index: number) => (
                     <div key={`${content.title}-${section.title}-${index}`} className="p-4 text-2xl bg-green-786 text-white bg-opacity-35">{sectionParagraph}</div>
                   ))}
@@ -25,7 +24,6 @@ export default function Home() {
               ))}
             </div>
           </div>
-        </div>
       </div>
     </div>
   );
